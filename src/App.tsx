30f809894f69dbd25eb437afc9eaec638a3660f8
BlackPri0r <lawrenceli630@gmail.com>
2024-10-17 04:18:07 +1300

    added data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+
 function App() {
 
   return (
     <>
       <div className="align-center">
 
-        <div className="bg-white m-5">
-          hello
+        <div className="bg-cardBg mt-20 mx-6">
+          <div className="bg-profile p-7">
+            <img src="public\image-jeremy.png" className="border-white"></img>
+            <div className="flex justify-center align-center">
+              <div className="">Report for</div>
+              <div className="">Jeremy Robinson</div>
+            </div>
+          </div>
+          <div className="text-white">
+            hello
+          </div>
         </div>
 
-
-
-
+        
       </div>
     </>
   )
